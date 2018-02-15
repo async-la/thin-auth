43009f17a0ff77990fd16f48c95291905a45b1bd
Zack Story <zack@root-two.com>
2018-02-15 14:50:44 -0800

    (server) twilio channel (#18) (#19)
    * keypair basics
    
    * make signing opt in
    
    * twilio
    
    * fix

--- packages/server/src/db/index.js
+++ packages/server/src/db/index.js
@@ -2,8 +2,6 @@
 
 import Sequelize from "sequelize"
 
-import type { TenantType } from '@rt2zz/thin-auth-interface'
-
 const defaultConfig = {
   freezeTableName: true
 }
@@ -28,9 +26,12 @@ function createSequelize(tenant: TenantType) {
   const Alias = sequelize.define(
     `${tenantId}:alias`,
     {
-      email: {
+      credential: {
         type: Sequelize.STRING,
         primaryKey: true
+      },
+      type: {
+        type: Sequelize.STRING,
       },
       userId: {
         type: Sequelize.STRING
@@ -94,12 +95,27 @@ export const Tenant = rootSequelize.define(
     authVerifyUrl: {
       type: Sequelize.STRING
     },
+    twilioConfig: { 
+      type: Sequelize.JSON,
+    }
   },
   {
     freezeTableName: true,
     timestamps: false,
   }
 )
+
+export type TenantType = {
+  id: string,
+  name: string,
+  key: string,
+  authVerifyUrl: string,
+  twilioConfig: {
+    fromNumber: string,
+    sid: string,
+    authToken: string,
+  }
+}
 
 // sequelize.sync({ force: true })
 let sequelizeMap: Map<string, Object> = new Map()
