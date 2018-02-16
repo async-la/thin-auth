a90ed640b787fa0f31a5111ca7ff028ca660263e
rt2zz <zack@root-two.com>
2018-02-16 10:08:25 -0800

    (ops): fix babel stuff

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,7 +2,7 @@ const packageConfig = require("./package.json")
 
 // Babel, config from package.json + ignore non-@justgolift node_modules
 const babelConfig = Object.assign({}, packageConfig.babel, {
-  ignore: /node_modules\/(?!(edonode)\/.*)/
+  ignore: /node_modules\/(?!(edonode|@rt2zz)\/.*)/
 })
 
 require("babel-register")(babelConfig)
