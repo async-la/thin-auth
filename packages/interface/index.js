e124d2987df75bb34b8774f17c1d35bd8b53f346
rt2zz <zack@root-two.com>
2018-02-15 20:57:56 -0800

    (interface): rework exports

--- packages/interface/index.js
+++ packages/interface/index.js
@@ -1,4 +1,44 @@
 // @flow
-
 export * from './constants'
-export type * from './types'
+
+type ConnectionId = string
+export type Keypair = {
+  secretKey: Buffer,
+  publicKey: Buffer,
+}
+export type Signature = {
+  sign: Buffer,
+  publicKey: Buffer,
+}
+
+type CredentialType = 'email' | 'sms'
+export type AuthReq = { type: CredentialType, credential: string }
+
+export type ThinAuthServerApi = {|
+  approveAuth: ConnectionId => Promise<void>,
+  rejectAuth: ConnectionId => Promise<void>,
+  revokeAuth: ConnectionId => Promise<void>,
+  requestAuth: AuthReq => Promise<void>,
+  refreshIdWarrant: string => Promise<string>,
+
+  crypto_sign_keypair: () => Promise<Keypair>,
+  cryptoSign: (message: Buffer, secretKey: Buffer) => Promise<Signature>,
+  cryptoVerify: (signature: Signature) => Promise<string>,
+|}
+
+export type ThinAuthClientApi = {|
+  onAuthApprove: ({ idWarrant: string }) => Promise<void>
+|}
+
+export type SessionType = {
+  id: string,
+  userId: string,
+  connectionId: string
+}
+
+export type TenantType = {
+  id: string,
+  name: string,
+  key: string,
+  authVerifyUrl: string,
+}
