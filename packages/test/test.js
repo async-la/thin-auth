7ba158e19d4efd58faaca14dcf6b3f8693843c1f
Carlo C <carlo.cajucom@gmail.com>
2018-02-22 15:27:55 -0800

    init test

--- packages/test/test.js
+++ packages/test/test.js
@@ -5,16 +5,49 @@ import { AsyncStorage } from 'AsyncStorage'
 
 import createAuthClient from '../client'
 
-test('createAuthClient', async t => {
-  const { authReset, authRemote, refreshIdWarrant } = createAuthClient({
+// Stub session id generation
+// $FlowFixMe
+Math.random = () => 1
+const sessionIdCipher = '6e'
+
+function setup() {
+  const client = createAuthClient({
     endpoint: 'wss://api.auth.asy.nc',
     apiKey: 'soniuf39j3fsxxxe-dev',
-    onAuthApprove: async () => console.log('approved'),
+    onAuthApprove: async () => console.log('!! Auth Approved'),
     storage: AsyncStorage,
   })
-  console.log(authRemote)
+  return client
+}
+
+test('refresh id warrant when not verified', async t => {
+  const { refreshIdWarrant } = setup()
+  await t.throws(refreshIdWarrant())
+})
+
+test('refresh id warrant when verified', async t => {
+  const { authRemote, refreshIdWarrant } = setup()
   const api = await authRemote()
-  console.log(api)
   await api.requestAuth({ type: 'email', credential: 'carlo.cajucom+test@gmail.com' })
-  t.pass()
+  await api.approveAuth(sessionIdCipher)
+  await t.notThrows(refreshIdWarrant())
+})
+
+test('refresh id warrant when rejected', async t => {
+  const { authRemote, refreshIdWarrant } = setup()
+  const api = await authRemote()
+  await api.requestAuth({ type: 'email', credential: 'carlo.cajucom+test@gmail.com' })
+  await api.rejectAuth(sessionIdCipher)
+  await t.throws(refreshIdWarrant())
+})
+
+test.only('refresh id when reset', async t => {
+  const { authReset, authRemote, refreshIdWarrant } = setup()
+  const api = await authRemote()
+  await api.requestAuth({ type: 'email', credential: 'carlo.cajucom+test@gmail.com' })
+  await api.approveAuth('6e')
+  // @TODO: switch to auth reset, AsyncStorage does not have `removeItem` (carlo)
+  await authReset()
+  //await api.revokeAuth('1')
+  await t.throws(refreshIdWarrant())
 })
