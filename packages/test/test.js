6e311e6d96b15d08fd33fa4e9573d1b1ac2c3d10
Carlo C <carlo.cajucom@gmail.com>
2018-02-22 16:07:27 -0800

    crypto tests

--- packages/test/test.js
+++ packages/test/test.js
@@ -13,7 +13,7 @@ const sessionIdCipher = '6e'
 function setup() {
   const client = createAuthClient({
     endpoint: 'wss://api.auth.asy.nc',
-    apiKey: 'soniuf39j3fsxxxe-dev',
+    apiKey: 'test',
     onAuthApprove: async () => console.log('!! Auth Approved'),
     storage: AsyncStorage,
   })
@@ -41,7 +41,7 @@ test('refresh id warrant when rejected', async t => {
   await t.throws(refreshIdWarrant())
 })
 
-test.only('refresh id when reset', async t => {
+test('refresh id when reset', async t => {
   const { authReset, authRemote, refreshIdWarrant } = setup()
   const api = await authRemote()
   await api.requestAuth({ type: 'email', credential: 'carlo.cajucom+test@gmail.com' })
@@ -50,4 +50,23 @@ test.only('refresh id when reset', async t => {
   await authReset()
   //await api.revokeAuth('1')
   await t.throws(refreshIdWarrant())
+})
+
+test('crypto sign with secret key', async t => {
+  const { authReset, authRemote, refreshIdWarrant } = setup()
+  const api = await authRemote()
+  const kp = await api.crypto_sign_keypair()
+  const message = 'the message with correct secret key'
+  const signedMessage = await api.cryptoSign(Buffer.from(message, 'ascii'), kp.secretKey)
+  const verifiedMessage = await api.cryptoVerify(signedMessage)
+  t.is(verifiedMessage, message)
+})
+
+test('crypto sign with wrong secret key', async t => {
+  const { authReset, authRemote, refreshIdWarrant } = setup()
+  const api = await authRemote()
+  const message = 'the message with wrong secret key'
+  const signedMessage = await api.cryptoSign(Buffer.from(message, 'ascii'), Buffer.from('badKey', 'ascii'))
+  const verifiedMessage = await api.cryptoVerify(signedMessage)
+  t.not(verifiedMessage, message)
 })
