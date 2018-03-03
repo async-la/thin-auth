088bf3b7ba1df88b692847d540aba5e55c24c64f
rt2zz <zack@root-two.com>
2018-03-03 01:29:31 -0800

    (deps): bump deps on server

--- scripts/release.js
+++ scripts/release.js
@@ -1,13 +1,20 @@
-let execSync = require("child_process").execSync
+let execSync = require("child_process").execSync;
 
-execSync(`npm version patch`, { stdio: [0, 1, 2] })
-let v = require('../package.json').version
-execSync(`npm version ${v}`, { cwd: 'packages/interface', stdio: [0, 1, 2] })
-execSync(`npm version ${v}`, { cwd: 'packages/client', stdio: [0, 1, 2] })
+execSync(`npm version patch`, { stdio: [0, 1, 2] });
+let v = require("../package.json").version;
+execSync(`npm version ${v}`, { cwd: "packages/interface", stdio: [0, 1, 2] });
+execSync(`npm version ${v}`, { cwd: "packages/client", stdio: [0, 1, 2] });
 
-execSync(`npm publish`, { cwd: 'packages/interface', stdio: [0, 1, 2] })
-execSync(`npm i @rt2zz/thin-auth-interface@${v}`, { cwd: 'packages/client', stdio: [0, 1, 2] })
-execSync(`npm publish`, { cwd: 'packages/client', stdio: [0, 1, 2] })
+execSync(`npm publish`, { cwd: "packages/interface", stdio: [0, 1, 2] });
+execSync(`npm i @rt2zz/thin-auth-interface@${v}`, {
+  cwd: "packages/client",
+  stdio: [0, 1, 2]
+});
+execSync(`npm i @rt2zz/thin-auth-interface@${v}`, {
+  cwd: "packages/server",
+  stdio: [0, 1, 2]
+});
+execSync(`npm publish`, { cwd: "packages/client", stdio: [0, 1, 2] });
 
-execSync(`git commit -am '(release): v${v}'`, { stdio: [0, 1, 2] })
-execSync(`git push origin`, { stdio: [0, 1, 2] })
+execSync(`git commit -am '(release): v${v}'`, { stdio: [0, 1, 2] });
+execSync(`git push origin`, { stdio: [0, 1, 2] });
