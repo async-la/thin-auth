4dafcd6be25551e672ebe5539a1e38c7381c14ad
rt2zz <zack@root-two.com>
2018-03-10 18:04:34 -0800

    (crypto): fix sign / verify mode

---
+++ packages/test/__tests__/crypto-full.test.js
@@ -0,0 +1,19 @@
+// @flow
+import test from "ava";
+import { setupClient } from "./_helpers";
+
+test.beforeEach(t => {
+  t.context.client = setupClient({
+    onDevRequest: c => {
+      t.context.cipher = c;
+    },
+    sign: true
+  });
+});
+
+test("siging works", async t => {
+  const { authRemote, refreshIdWarrant } = t.context.client;
+  const api = await authRemote();
+  await api.requestAuth({ type: "dev", credential: "dev-credential" });
+  t.true(!!t.context.cipher);
+});
